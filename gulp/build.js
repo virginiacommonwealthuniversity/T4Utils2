3f3d8f277787528e3dcca967df9ac0fd90d52e08
Joel Eisner <jeisner93@gmail.com>
2016-10-31 14:04:13 -0400

    Updated the gulp/build module to provide better comment documentation and indentation

--- gulp/build.js
+++ gulp/build.js
@@ -31,22 +31,22 @@ var api = {
 
 module.exports = function(gulp, config, version) {
     gulp.task('build-' + version, function() {
-        return gulp.src(config.src)                           // Grab the source files
-            .pipe(include())                                  // Include javascript modules
-            .pipe(replace(/\{\{version\}\}/g, pkg.version))   // Replace {{version}} with the package.json version
-            .pipe(replace(/\{\{t4_version\}\}/g, version))    // Replace {{t4_version}} with the passed in version
-            .pipe(replace(/\{\{datestamp\}\}/g, datestamp())) // Replace {{datestamp}} with a YYYY.mm.dd datestamp
-            .pipe(replace(/'\{\{api:content_type_id\}\}'/g, api.content_type_id[version]))
-            .pipe(replace(/'\{\{api:ocm\}\}'/g, api.ocm[version]))
-            .pipe(replace(/'\{\{api:ocm_get_id\}\}'/g, api.ocm_get_id[version]))
-            .pipe(replace(/'\{\{api:ocm_next_content_type_id\}\}'/g, api.ocm_next_content_type_id[version]))
-            .pipe(replace(/'\{\{api:ocm_prev_content_type_id\}\}'/g, api.ocm_prev_content_type_id[version]))
-            .pipe(header(version))                                   // Inject library header
-            .pipe(rename({suffix:'.' + version}))             // Give the filename a version suffix
-            .pipe(gulp.dest(config.dest))                     // Save to the destination folder
-            .pipe(uglify())                                   // Uglify the code
-            .pipe(header(version))                                   // Re-inject library header
-            .pipe(rename({suffix:'.min'}))                    // Give the filename a .min suffix
-            .pipe(gulp.dest(config.dest));                    // Save to the destination folder
+        return gulp.src(config.src)                                                                          // Grab the source files
+            .pipe(include())                                                                                 // Include javascript modules
+            .pipe(replace(/\{\{version\}\}/g, pkg.version))                                                  // Replace {{version}} with the package.json version
+            .pipe(replace(/\{\{t4_version\}\}/g, version))                                                   // Replace {{t4_version}} with the passed in version
+            .pipe(replace(/\{\{datestamp\}\}/g, datestamp()))                                                // Replace {{datestamp}} with a YYYY.mm.dd datestamp
+            .pipe(replace(/'\{\{api:content_type_id\}\}'/g, api.content_type_id[version]))                   // Replace {{api:content_type_id}} with the version specific JS
+            .pipe(replace(/'\{\{api:ocm\}\}'/g, api.ocm[version]))                                           // Replace {{api:ocm}} with the version specific JS
+            .pipe(replace(/'\{\{api:ocm_get_id\}\}'/g, api.ocm_get_id[version]))                             // Replace {{api:ocm_get_id}} with the version specific JS
+            .pipe(replace(/'\{\{api:ocm_next_content_type_id\}\}'/g, api.ocm_next_content_type_id[version])) // Replace {{api:ocm_next_content_type_id}} with the version specific JS
+            .pipe(replace(/'\{\{api:ocm_prev_content_type_id\}\}'/g, api.ocm_prev_content_type_id[version])) // Replace {{api:ocm_prev_content_type_id}} with the version specific JS
+            .pipe(header(version))                                                                           // Inject library header
+            .pipe(rename({suffix:'.' + version}))                                                            // Give the filename a version suffix
+            .pipe(gulp.dest(config.dest))                                                                    // Save to the destination folder
+            .pipe(uglify())                                                                                  // Uglify the code
+            .pipe(header(version))                                                                           // Re-inject library header
+            .pipe(rename({suffix:'.min'}))                                                                   // Give the filename a .min suffix
+            .pipe(gulp.dest(config.dest));                                                                   // Save to the destination folder
     });
 };
