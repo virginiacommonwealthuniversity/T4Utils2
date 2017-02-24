69ab6b8233933cfcf3a7e5a72b70ab91d2de2729
Joel Eisner <jeisner93@gmail.com>
2017-02-24 12:51:19 -0500

    Cleaned up build-system files to utilize ES2015 (ES6) and updated the linting files

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,19 @@
-// Required Modules
-var build = require('./gulp/build.js'),
-    config = require('./gulp/config.js'),
-    gulp = require('gulp'),
-    jsdoc = require('gulp-jsdoc3');
+let build =  require('./gulp/build'),
+    config = require('./gulp/config'),
+    gulp =   require('gulp'),
+    jsdoc =  require('gulp-jsdoc3');
 
-// Default Task
 gulp.task('default', ['build']);
 
-// Build Task
 gulp.task('build', ['build-8.1', 'build-7.4']); // Build both versions of the library
+
 build(gulp, config, '8.1');                     // Build the v8 version of the library
 build(gulp, config, '7.4');                     // Build the v7 version of the library
 
-// Watch Task
 gulp.task('watch', function() {
     gulp.watch(config.watch, ['build']);        // Run the build task
 });
 
-// Docs Task
 gulp.task('docs', ['build'], function(doc) {
     gulp.src('dist/T4Utils.js', {read: false})
         .pipe(jsdoc(doc));
