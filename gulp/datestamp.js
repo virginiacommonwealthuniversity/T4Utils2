9cc648ddcdf111cd6a520b99ee7886f6d2f180bd
Joel Eisner <jeisner93@gmail.com>
2017-03-17 15:23:58 -0400

    Dropped T4 7.4 support

--- gulp/datestamp.js
+++ gulp/datestamp.js
@@ -1,9 +1,11 @@
 // Export
-module.exports = function () {
+module.exports = () => {
     let today = new Date(),
         month = leftPad(today.getMonth() + 1),
         day =   leftPad(today.getDate()),
         year =  today.getFullYear();
     return year + '.' + month + '.' + day;
-    function leftPad(int) {return int < 10 ? '0' + int : int;}
+    function leftPad(int) {
+        return int < 10 ? '0' + int : int;
+    }
 };
--- gulp/header.js
+++ gulp/header.js
@@ -3,18 +3,17 @@ let datestamp = require('./datestamp.js'),
     header =    require('gulp-header'),
     pkg =       require('../package.json');
 
-module.exports = function(version) {
+module.exports = () => {
     let banner =
 `/**
  * T4Utils 2
  * @author <%= pkg.author %>
- * @version <%= pkg.version %>_<%= info.date %> (<%= info.version %>)
+ * @version <%= pkg.version %>_<%= info.date %>
  * @license <%= pkg.license %>
  */
 
 `,
         info = {
-            version: version,
             date: datestamp()
         };
     return header(banner, {pkg, info});
