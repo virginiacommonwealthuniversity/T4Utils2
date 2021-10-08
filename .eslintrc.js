b977762e29bc6a8803c3462d42a3c8cd6102c6c6
Joel Eisner <jeisner93@gmail.com>
2021-10-08 13:22:01 -0400

    Webpack & package cleanup
    Installed webpack and set up an initial configuration for
    building/developing the library. In addition, ESLint is installed as a
    development dependency and its configuration has been updated.

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
+    },
+    extends: 'eslint:recommended',
+    globals: {
+        'com': true,
+        'content': true,
+        'ContentHierarchy': true,
+        'ContentManager': true,
+        'currentSection': true,
+        'dbStatement': true,
+        'importClass': true,
+        'importPackage': true,
+        'isPreview': true,
+        'java': true,
+        'language': true,
+        'MediaManager': true,
+        'MediaUtils': true,
+        'MessageDigest': true,
+        'PathBuilder': true,
+        'publishCache': true,
+        'section': true,
+        'T4Utils': true,
+        'TreeTraversalUtils': true
+    },
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module'
+    },
+    rules: {
+        indent: ['warn', 4],
+        'linebreak-style': ['warn', 'unix'],
+        quotes: ['warn', 'single'],
+        semi: ['warn', 'always'],
+        'valid-jsdoc': ['error']
+    }
+};
---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const mode = process.env.NODE_ENV;
+
+module.exports = {
+    entry: './index.js',
+    mode,
+    module: {
+        rules: [
+            {
+                exclude: /node_modules/,
+                test: /\.js$/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    output: {
+        filename: 'T4Utils.js',
+        path: path.join(__dirname, 'dist')
+    },
+    plugins: [
+        new TerserPlugin()
+    ],
+    target: ['web', 'es5']
+};
