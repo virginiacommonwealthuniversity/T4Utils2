8c88883c54e27c166b4fbea2c6ca848837721222
Joel Eisner <jeisner93@gmail.com>
2021-10-08 15:11:18 -0400

    Base module and entry updates
    Updated `index.js` and `modules/base.js` to start thr process of
    upgrading the library to the new build system. In addition, changes were
    made to the base module for efficiency/readability:
    
    1. `console` methods use `this` instead of using redundant code.
    2. `console` and `write` will include a `t4utils` attribute on their
    rendered HTML elements.
    3. `context`, `contextIsPage`, and `contextIsContent` no longer use
    self-executing anonymous functions and have simpler code to accomplish
    the same functionality.

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
-//=require ./modules/dependencies.js
-//=require ./modules/base.js
-//=require ./modules/*
+/**
+ * T4Utils
+ * @module
+ * @contributors Ben Margevicius <bdm4@case.edu>, Joel Eisner <eisnerjr@vcu.edu>
+ * @version 2.0.0
+ */
+
+import * as base from './modules/base';
+
+export default {
+    ...base
+};
--- modules/base.js
+++ modules/base.js
@@ -1,142 +1,141 @@
 /**
- * T4Utils
- * @module
- * @author Ben Margevicius <bdm4@case.edu>, Joel Eisner <eisnerjr@vcu.edu>
- * @version 1.2.0
+ * Base module
+ * @module modules/base
+ * @contributors Ben Margevicius <bdm4@case.edu>, Joel Eisner <eisnerjr@vcu.edu>
+ * @version 2.0.0
  */
-var T4Utils = (function (utils) {
 
-    /**
-     * The version of this library
-     * @member version
-     * @returns {string} the current version
-     * @example
-     * T4Utils.version;
-     */
-    utils.version = '{{version}}_{{datestamp}}';
+// Grab the package information
+import pkg from '../package.json';
 
-    /**
-     * Writes the specified message type to the browser console
-     * @member console
-     * @param {string} consoleMethod - You can specify which console method you want to use. 'log, warn, error' are valid
-     * @param {String|Object} textOrObj - The text you want to write to the screen. With the console method you should be able to write objects as well, but it's not the case from inside the Util class
-     * @returns {null} document.write's a script tag with a console method
-     * @example
-     * T4Utils.console(string, string);
-     */
-    utils.console = function (consoleMethod, textOrObj) {
-        if (typeof textOrObj === 'string') document.write('<script>console.' + consoleMethod + '("' + textOrObj + '");</script>\n');
-    };
+// Creates a toJavaString prototype on the global String object
+String.prototype.toJavaString = function () {
+    return new java.lang.String(this);
+};
 
-    /**
-     * Writes a message to the browser console
-     * @function console.log
-     * @param {String|Object} textOrObj - The text you want to write to the screen. With the console method you should be able to write objects as well, but it's not the case from inside the Util class
-     * @returns {null} document.write's a script tag with a console.log method
-     * @example
-     * T4Utils.console.log(string);
-     */
-    utils.console.log = function (textOrObj) {
-        if (typeof textOrObj === 'string') document.write('<script>console.log("' + textOrObj + '");</script>\n');
-    };
+/**
+ * The version of this library
+ * @member version
+ * @returns {string} the current version
+ * @example
+ * T4Utils.version;
+ */
+export const version = pkg.version;
 
-    /**
-     * Writes a warning to the browser console
-     * @function console.warn
-     * @param {String|Object} textOrObj - The text you want to write to the screen. With the console method you should be able to write objects as well, but it's not the case from inside the Util class
-     * @returns {null} document.write's a script tag with a console.warn method
-     * @example
-     * T4Utils.console.warn(string);
-     */
-    utils.console.warn = function (textOrObj) {
-        if(typeof textOrObj === 'string') document.write('<script>console.warn("' + textOrObj + '");</script>\n');
-    };
+/**
+ * Writes the specified message type to the browser console
+ * @member console
+ * @param {String} method - You can specify which console method you want to use. 'log, warn, error' are valid
+ * @param {String} message - The text you want to write to the screen.
+ * @returns {null} document.write's a script tag with a console method
+ * @example
+ * T4Utils.console(string, string);
+ */
+export function console(method, message) {
+    // If the type of message isn't a string, do nothing else
+    if (typeof message !== 'string') return;
 
-    /**
-     * Writes an error to the browser console
-     * @function console.error
-     * @param {String|Object} textOrObj - The text you want to write to the screen. With the console method you should be able to write objects as well, but it's not the case from inside the Util class
-     * @returns {null} document.write's a script tag with a console.error method
-     * @example
-     * T4Utils.console.error(string);
-     */
-    utils.console.error = function (textOrObj) {
-        if(typeof textOrObj === 'string') document.write('<script>console.error("' + textOrObj + '");</script>\n');
-    };
+    // Otherwise, write a script tag with the console method
+    return document.write(`<script t4utils>console.${ method }("${ message }");</script>`);
+}
 
-    /**
-     * Sets the context of where the library is being executed from (i.e. null if from within a page-layout, content if from within a content-type)
-     * @member context
-     * @return {?Object} context - Either null if the context is from within a page-layout, and the content variable if from withing a content-type
-     * T4Utils.context
-     */
-    utils.context = (function () {
-        return typeof content == 'undefined' ? null : content;
-    })();
+/**
+ * Writes a message to the browser console
+ * @function console.log
+ * @param {String} message - The text you want to write to the screen.
+ * @returns {null} document.write's a script tag with a console.log method
+ * @example
+ * T4Utils.console.log(string);
+ */
+console.log = function(message) {
+    return this('log', message);
+};
 
-    /**
-     * Boolean of whether the context of where the library is being executed from is within a page-layout
-     * @member contextIsPage
-     * @return {boolean} context - True if the library is executed within a page-layout, false if not
-     * @example
-     * T4Utils.contextIsPage
-     */
-    utils.contextIsPage = (function () {
-        return typeof content == 'undefined' ? true : false;
-    })();
+/**
+ * Writes a warning to the browser console
+ * @function console.warn
+ * @param {String} message - The text you want to write to the screen.
+ * @returns {null} document.write's a script tag with a console.warn method
+ * @example
+ * T4Utils.console.warn(string);
+ */
+console.warn = function(message) {
+    return this('warn', message);
+};
 
-    /**
-     * Boolean of whether the context of where the library is being executed from is within a content-type
-     * @member contextIsContent
-     * @return {boolean} context - True if the library is executed within a content-type, false if not
-     * @example
-     * T4Utils.contextIsContent
-     */
-    utils.contextIsContent = (function () {
-        return typeof content == 'undefined' ? false : true;
-    })();
+/**
+ * Writes an error to the browser console
+ * @function console.error
+ * @param {String} message - The text you want to write to the screen.
+ * @returns {null} document.write's a script tag with a console.error method
+ * @example
+ * T4Utils.console.error(string);
+ */
+console.error = function(message) {
+    return this('error', message);
+};
 
-    /**
-     * Writes a paragraph formatted HTML message to the browser
-     * @member write
-     * @param {string} text - The text you want to write to the screen
-     * @returns {null} document.write's a paragraph tag
-     * @example
-     * T4Utils.write(string);
-     */
-    utils.write = function (text) {
-        document.write('<p>' + text + '</p>\n');
-    };
+/**
+ * Sets the context of where the library is being executed from (i.e. null if from within a page-layout, content if from within a content-type)
+ * @member context
+ * @return {?Object} context - Either null if the context is from within a page-layout, and the content variable if from withing a content-type
+ * T4Utils.context
+ */
+export const context = content || null;
 
-    /**
-     * Converts a javascript object to Java string
-     * @member toString
-     * @param {object} obj - The object you want to convert
-     * @return {java.lang.String} the converted object
-     * @example
-     * T4Utils.toString(obj);
-     */
-    utils.toString = function (obj) {return new java.lang.String(obj);};
+/**
+ * Boolean of whether the context of where the library is being executed from is within a page-layout
+ * @member contextIsPage
+ * @return {boolean} context - True if the library is executed within a page-layout, false if not
+ * @example
+ * T4Utils.contextIsPage
+ */
+export const contextIsPage = !context;
 
-    /**
-     * Returns safe versions of HTML unsafe strings
-     * @member escapeHtml
-     * @param {string} unsafe - The unsafe string
-     * @return {string} an HTML safe string
-     * @example
-     * T4Utils.escapeHTML(string);
-     */
-    utils.escapeHtml = function (unsafe) {
-        return unsafe.replace(/&/g, '&amp;')
-            .replace(/</g, '&lt;')
-            .replace(/>/g, '&gt;')
-            .replace(/'/g, '&#039;');
-    };
+/**
+ * Boolean of whether the context of where the library is being executed from is within a content-type
+ * @member contextIsContent
+ * @return {boolean} context - True if the library is executed within a content-type, false if not
+ * @example
+ * T4Utils.contextIsContent
+ */
+export const contextIsContent = !!context;
 
-    // Creates a toJavaString prototype on the global String object
-    String.prototype.toJavaString = function () {return new java.lang.String(this);};
+/**
+ * Writes a paragraph formatted HTML message to the browser
+ * @member write
+ * @param {string} text - The text you want to write to the screen
+ * @returns {null} document.write's a paragraph tag
+ * @example
+ * T4Utils.write(string);
+ */
+export function write(text) {
+    document.write(`<p t4utils>${ text }</p>`);
+}
 
-    // Return the utils
-    return utils;
+/**
+ * Converts a javascript object to Java string
+ * @member toString
+ * @param {object} obj - The object you want to convert
+ * @return {java.lang.String} the converted object
+ * @example
+ * T4Utils.toString(obj);
+ */
+export function toString(obj) {
+    return new java.lang.String(obj);
+}
 
-})(T4Utils || {});
+/**
+ * Returns safe versions of HTML unsafe strings
+ * @member escapeHtml
+ * @param {string} unsafe - The unsafe string
+ * @return {string} an HTML safe string
+ * @example
+ * T4Utils.escapeHTML(string);
+ */
+export function escapeHtml(unsafe) {
+    return unsafe.replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/'/g, '&#039;');
+}
