aba6b196d199ef9776b5ce82ac6019f1e48a693d
Joel Eisner <jeisner93@gmail.com>
2021-10-11 12:38:09 -0400

    Webpack config changes
    Updated the `webpack.config.js` to include a banner at the top of the
    build file and to change the stats mode whether in
    development/production.

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
+const { BannerPlugin } = require('webpack');
 const path = require('path');
 const TerserPlugin = require('terser-webpack-plugin');
+const {
+    version,
+    license,
+    author
+} = require('./package.json');
 
 const mode = process.env.NODE_ENV;
 
 module.exports = {
-    entry: './index.js',
+    entry: path.join(__dirname, 'index.js'),
     mode,
     module: {
         rules: [
@@ -25,7 +31,13 @@ module.exports = {
         path: path.join(__dirname, 'dist')
     },
     plugins: [
-        new TerserPlugin()
+        new TerserPlugin({
+            extractComments: false
+        }),
+        new BannerPlugin({
+            banner: `T4Utils2 v${ version } | ${ license } | ${ author }`
+        })
     ],
+    stats: mode === 'production' ? 'summary' : true,
     target: ['web', 'es5']
 };
